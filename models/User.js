bb2e9c828bcea739551c22917ed190c5d3d15c66
hyinnnnn <91788526+hyinnnnn@users.noreply.github.com>
2023-12-04 03:36:35 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -46,6 +46,16 @@ module.exports = class User extends Sequelize.Model{
             collate: 'utf8_general_ci',
         });
     }
+
+    static associate(models) {
+        // User 모델과 다른 모델들 간의 관계 설정
+        this.hasMany(models.Content, { foreignKey: 'userId' });
+        this.hasMany(models.Discount, { foreignKey: 'userId' });
+        this.hasMany(models.Payment, { foreignKey: 'userId' });
+        this.hasMany(models.Review, { foreignKey: 'userId' });
+        this.belongsToMany(models.Movie, { through: 'UserMovie', foreignKey: 'userId' });
+    }
+    
     // // 비밀번호 검증 메서드
     // validPassword(password) {
     //     return bcrypt.compareSync(password,this.getDataValue('password'));
